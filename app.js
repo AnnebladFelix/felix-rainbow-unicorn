366e438ae1adc8548688a8e9e25f9a96deb5153e
Felix Anneblad <felix.anneblad@gmail.com>
2023-01-16 13:02:44 +0100

    new object

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ typeOfAnimal = [
     'giraff'
 ];
 console.log(typeOfAnimal);
+// let string1 = "strängen";
+// let number1 = 22;
+// console.log(string1.indexOf)
+// console.log(number1.valueOf)
+// let testFoo = function(foo){
+// }
+var testObj = {
+    string1: "sträng",
+    number1: 22,
+    boolean1: false
+};
+console.log(testObj.number1.valueOf);
+console.log(testObj.string1.indexOf);
+console.log(testObj.boolean1);
+console.log(testObj);
--- app.ts
+++ app.ts
@@ -9,3 +9,17 @@ typeOfAnimal = [
 
 console.log(typeOfAnimal);
 
+ type testObj = {
+    string1: string,
+    number1: number,
+    boolean1: boolean
+}
+
+
+let testObj: {string1: string, number1: number, boolean1: boolean} = {
+    string1: "sträng",
+    number1: 22,
+    boolean1: false,
+}
+
+console.log(testObj)
